15371612af96f0e42aed9675eb7e0f98b45f5942
skasture723 <sara.kasture@ufl.edu>
2023-03-11 14:10:27 -0500

    commit1

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 def encode(num):
     result = ""
     num = str(num)
-    for i in num:
-        i = int(i)+3
+    for i in num: # go through all i in string num
+        i = int(i)+3 # convert i to integer and add 3 to encode
         result = result + str(i)
     return result
 
 def decode (num):
     result = ""
     num = str(num)
-    for i in num:
-        i = int(i)-3
+    for i in num: # go through all i in string num
+        i = int(i)-3 # convert i to integer and subtract 3 to decode
         result = result + str(i)
     return result
 
@@ -23,8 +23,9 @@ if __name__ == "__main__":
         menu_option = int(input("\nPlease enter an option: "))
         if menu_option == 1:
             num = input("Please enter your password to encode: ")
-            encoded = encode(num)
+            encoded = encode(num) # encodes the input
             print ("Your password has been encoded and stored!\n")
         elif menu_option == 2:
+            # print the result
             print ("The encoded password is " + str(encoded) + ", and the original password is " + str(num) + ".\n")
 
